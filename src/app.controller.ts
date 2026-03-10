da1ccab60d14b487d9e8152e272fc32c94ea4eca
Dmitry <kabanooovda@gmail.com>
2026-03-10 18:40:07 +0300

    connected swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Описание первого ЭП' })
+  @ApiResponse({ status: 200, description: 'Приветственный ответ' })
+  @ApiResponse({ status: 403, description: 'Доступ запрещен' })
+  @ApiResponse({ status: 401, description: 'Не авторизован' })
+  @ApiResponse({ status: 403, description: 'Доступ запрещен' })
   getHello(): string {
     return this.appService.getHello();
   }
@@ -15,3 +21,5 @@ export class AppController {
     return this.appService.getHealth();
   }
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,25 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  
+
+  const config = new DocumentBuilder()
+    .setTitle("Шлюз проекта car-rent")
+    .setDescription("Полное описание всех ЭП")
+    .setVersion("1.0")
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
   // Включаем CORS
   app.enableCors();
-  
-  const port = process.env.PORT || 3000;
+
+  const port = process.env.PORT || 5000;
   await app.listen(port);
-  
+
   console.log(`Gateway running on: http://localhost:${port}`);
 }
 bootstrap();
